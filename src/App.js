550312681baf04e43e63f16576533bd71547fb62
DESKTOP-7F0UINR\kse <developeon80@gmail.com>
2018-09-28 00:15:42 +0900

    Loading states 구현

--- src/App.js
+++ src/App.js
@@ -5,44 +5,41 @@ import Movie from './Movie';
 class App extends Component {
 
   state = {
-    movies : [
-      {
-        title : "신과함께",
-        poster : "http://newsimg.sedaily.com/2017/11/22/1ONOHF5TDF_2.jpg"
-      },
-      {
-        title : "너의결혼식",
-        poster : "http://img.khan.co.kr/news/2018/08/21/l_2018082101002297200189602.jpg"
-      },
-      {
-        title : "안시성",
-        poster : "http://thumbnail.egloos.net/600x0/http://pds27.egloos.com/pds/201808/25/74/d0014374_5b8118bd62f3c.jpg"
-      },
-    ]
   }
 
   componentDidMount(){
     setTimeout(()=>{
       this.setState({
         movies : [
-          ...this.state.movies,
           {
-            title : "늑대소년",
-            poster : "http://image.chosun.com/sitedata/image/201210/14/2012101401034_0.jpg"
-          }
+            title : "신과함께",
+            poster : "http://newsimg.sedaily.com/2017/11/22/1ONOHF5TDF_2.jpg"
+          },
+          {
+            title : "너의결혼식",
+            poster : "http://img.khan.co.kr/news/2018/08/21/l_2018082101002297200189602.jpg"
+          },
+          {
+            title : "안시성",
+            poster : "http://thumbnail.egloos.net/600x0/http://pds27.egloos.com/pds/201808/25/74/d0014374_5b8118bd62f3c.jpg"
+          },
         ]
       })
     }, 1000)
   }
 
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    })
+    console.log(movies);
+    return movies
+  }
+
   render() {
     return (
       <div className="App">
-        {
-          this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index}/>
-          })
-        }
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
